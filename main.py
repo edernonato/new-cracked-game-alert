6d5ef9836dd5ef1c3c287c8c1d1e302f62e3f52b
unknown <edernonato@outlook.com>
2023-01-10 20:59:04 -0300

    check of the page updated, if there are more than one new game in the page, the script will check, and send by email all the new games

--- main.py
+++ main.py
@@ -22,40 +22,56 @@ connection.login(user=email_from, password=password)
 email_to = "edernonato@outlook.com"
 
 soup = BeautifulSoup(website, "html.parser")
-last_game = soup.find(name="h2").text
-last_game_link = soup.find(name="h2").a.get('href')
-image_tag = soup.find(name="img", class_="aligncenter")
-t = time.localtime()
+last_games = [game.text for game in soup.find_all(name="h2")]
+last_game_links = [link.a.get('href') for link in soup.find_all(name="h2")]
+image_tag = soup.select(selector="p a img")
+image_tag = [image.get("data-lazy-src") for image in image_tag if image.get("data-lazy-src") is not None]
 
-image_link = image_tag.get("data-lazy-src")
+t = time.localtime()
 html_start = f'''
     <html>
         <body>
-            <h4>New Cracked Game: {last_game} - {time.strftime("%H:%M:%S", t)}</h2>
-            <a href="{last_game_link}">{last_game}</a>
         '''
 html_end = '''
         </body>
     </html>
     '''
-img = f"<img src='{image_link}' alt='{last_game}'>"
+html_body = ""
 
-html = html_start + "\n" + img + "\n" + html_end
 
-print(html)
 with open("C:\\Users\\Eder\\PycharmProjects\\skidrow-last-cracked-game-alert\\last_game.txt", "r") as file:
-    if file.read() == last_game:
+    last_saved_game = file.read()
+    if last_saved_game == last_games[0]:
         print("SAME GAME")
     else:
+        if last_saved_game in last_games:
+            max_index = last_games.index(last_saved_game)
+        else:
+            max_index = len(last_games)
+
+        for game_index in range(max_index):
+            image_link = image_tag[game_index]
+            html_body += f"""
+                        <h4>New Cracked Game: {last_games[game_index]} - {time.strftime("%H:%M:%S", t)} <br><br></h4>
+                        <div>
+                        <a href="{last_game_links[game_index]}" style="text-decoration:none; color: #11999E;">{last_games[game_index]}</a>
+                        </div>
+                        <img src='{image_link}' alt='{last_games[game_index]}'></img>
+            """
         print("Writing new game and sending email")
         with open("C:\\Users\\Eder\\PycharmProjects\\skidrow-last-cracked-game-alert\\last_game.txt", "w") as file2:
-            file2.write(last_game)
+            file2.write(last_games[0])
 
+        if max_index == 0:
+            last_game = last_games[0]
+        else:
+            last_game = f"{max_index} new Games"
+        html = html_start + html_body + html_end
         email_message = MIMEMultipart()
         email_message['From'] = email_from
         email_message['To'] = email_to
         email_message['Subject'] = f'New Cracked Game: {last_game} - {date.today()}'
         email_message.attach(MIMEText(html, "html"))
         email_string = email_message.as_string()
-
         connection.sendmail(from_addr=email_from, to_addrs=email_to, msg=email_string)
+        print(html)
